c477ec7f8deb4e6904a4212114e0744161c50ab8
dickwyn <dickwyn@outlook.com>
2019-10-09 16:50:52 -0700

    (refactor) get exec member hook to return an array of objects

--- src/hooks/get-exec-members.js
+++ src/hooks/get-exec-members.js
@@ -1,4 +1,5 @@
 import { graphql, useStaticQuery } from "gatsby";
+import uuid from "uuid";
 
 const getExecutiveMembers = () => {
   const execs = useStaticQuery(graphql`
@@ -26,8 +27,21 @@ const getExecutiveMembers = () => {
     }
   `);
 
+  const arrExecs = [];
+
+  execs.allMarkdownRemark.edges.forEach(edge => {
+    arrExecs.push({
+      id: uuid.v4(),
+      title: edge.node.frontmatter.title,
+      role: edge.node.frontmatter.role,
+      photo: edge.node.frontmatter.photo,
+      link: edge.node.frontmatter.link,
+      description: edge.node.frontmatter.description,
+    });
+  });
+
   if (!execs) return null;
-  return execs;
+  return arrExecs;
 };
 
 export default getExecutiveMembers;
