7bb3dfd66b5249df527c812e8e33658d59bd798e
Nelson Dantis <dantisnelson@gmail.com>
2019-10-09 17:04:25 -0700

    Create event-data.js

---
+++ src/pages/event-data.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import getPartnerOrganization from "../hooks/get-event-data";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+const eventData = () => {
+  const data = getEventData();
+  return (
+    <Layout>
+      <SEO title="Events" />
+      <h1>Events</h1>
+      <p>Data</p>
+      {data.allMarkdownRemark.edges.map(post => (
+        <div key={post.node.id}>
+          <p>{post.node.frontmatter.title}</p>
+          <p>{post.node.frontmatter.email}</p>
+          <p>{post.node.frontmatter.photo}</p>
+          <p>{post.node.frontmatter.link}</p>
+          <p>{post.node.frontmatter.description}</p>
+        </div>
+      ))}
+      <Link to="/">Go back to the homepage</Link>
+    </Layout>
+  );
+};
+export default eventData;
