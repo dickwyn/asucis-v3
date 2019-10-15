7677e3cf5bb72123d1e7da4efa8ee199d0299288
dickwyn <dickwyn@outlook.com>
2019-10-14 20:03:07 -0700

    (update) executive team member query

--- src/pages/executive-members.js
+++ src/pages/executive-members.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import getExecutiveMembers from "../hooks/get-exec-members";
+import getExecutiveMembers from "../hooks/get-executive-member";
 
 const MemberPage = () => {
   const data = getExecutiveMembers();
