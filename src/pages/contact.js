90e17471099a2acf2904f005f8a49d6f4a906f01
dickwyn <dickwyn@outlook.com>
2019-10-16 20:27:56 -0700

    (feat) embed typeform into contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import * as typeformEmbed from "@typeform/embed";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+class Contact extends Component {
+  componentDidMount() {
+    const elm = this.typeformElm;
+    typeformEmbed.makeWidget(elm, "https://asucis.typeform.com/to/MtcZjL", {
+      hideScrollbars: true,
+      hideFooter: true,
+      onSubmit() {
+        console.log("Typeform successfully submitted");
+      },
+    });
+  }
+
+  render() {
+    return (
+      <Layout>
+        <SEO title="Contact" />
+        <div
+          className="ReactTypeformEmbed"
+          style={{ height: "100%" }}
+          ref={tf => {
+            this.typeformElm = tf;
+          }}
+        />
+      </Layout>
+    );
+  }
+}
+
+export default Contact;
