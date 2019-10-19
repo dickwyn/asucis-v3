d89060067b776c1b9404abf83d103ace1d9460bf
dickwyn <dickwyn@outlook.com>
2019-10-19 16:08:13 -0700

    (style) header for desktop view

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,28 +2,32 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "gatsby";
 import Image from "./image";
+import HeroImage from "./HeroImage";
 
 const Header = ({ siteTitle }) => (
-  <header>
-    <div className="container">
-      <div className="logo">
-        <Link to="/">
-          <Image alt={siteTitle} />
-        </Link>
-      </div>
-      <div className="navigation">
-        <div className="ticker">lorem ipsum dol somer text</div>
-        <div className="links">
-          <Link to="/">Home</Link>
-          <Link to="/page-2">About Us</Link>
-          <Link to="/executive-members">Our Team</Link>
-          <Link to="/event-data">Events</Link>
-          <Link to="/partnerorg">Our Partners</Link>
-          <Link to="/contact">Contact</Link>
+  <>
+    <nav>
+      <div className="navbar-container">
+        <div className="logo">
+          <Link to="/">
+            <Image alt={siteTitle} />
+          </Link>
+        </div>
+        <div className="navigation">
+          <div className="ticker">lorem ipsum dol somer text</div>
+          <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/page-2">About Us</Link>
+            <Link to="/executive-members">Our Team</Link>
+            <Link to="/event-data">Events</Link>
+            <Link to="/partnerorg">Our Partners</Link>
+            <Link to="/contact">Contact</Link>
+          </div>
         </div>
       </div>
-    </div>
-  </header>
+    </nav>
+    <HeroImage className="hero" />
+  </>
 );
 
 Header.propTypes = {
