687e01496ef5dc173c5858b8a81b111d2e849636
dickwyn <dickwyn@outlook.com>
2019-10-18 23:53:56 -0700

    (feat) create framework for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,22 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "gatsby";
+import Image from "./image";
 
 const Header = ({ siteTitle }) => (
   <header>
-    <div>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+    <div className="container">
+      <div className="logo">
+        <Image alt={siteTitle} />
+      </div>
+      <div className="navigation">
+        <Link to="/">Home</Link>
+        <Link to="/">About Us</Link>
+        <Link to="/">Our Team</Link>
+        <Link to="/">Events</Link>
+        <Link to="/">Our Partners</Link>
+        <Link to="/">Contact</Link>
+      </div>
     </div>
   </header>
 );
--- src/components/image.js
+++ src/components/image.js
@@ -2,14 +2,15 @@ import React from "react";
 import Img from "gatsby-image";
 import getOptimizedImage from "../hooks/get-optimized-image";
 
-const Image = () => {
+const Image = props => {
   const images = getOptimizedImage();
 
   const placeholder = images.edges.find(
-    image => image.node.relativePath === "asucis-icon.png"
+    image => image.node.relativePath === "logo.png"
   );
 
-  return <Img fluid={placeholder.node.childImageSharp.fluid} />;
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Img fluid={placeholder.node.childImageSharp.fluid} {...props} />;
 };
 
 export default Image;
